8cbb9a2815c5dbefdc396d0cf54e2150dc5d06e2
AJ <67955763+arsh985@users.noreply.github.com>
2021-11-12 11:41:53 +0530

    Update App.java

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Shaikh A j 985!" );
+        System.out.println( "Hello world!" );
     }
 }
